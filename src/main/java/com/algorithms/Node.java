00c39f44a8e679b4ac8a4f0858a882b082757a8d
ben-vow-repo <benjaminvowles2008@gmail.com>
2026-09-18 00:02:36 +0100

    fix: node constructor added visited variable

--- src/main/java/com/algorithms/Node.java
+++ src/main/java/com/algorithms/Node.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class Node {
     public String label;
     public List<Edge> edgeList;
+    public Boolean visited;
 
     public Node(String label){
         this.label = label;
         this.edgeList = new ArrayList<>();
+        this.visited = false;
     }
 }
