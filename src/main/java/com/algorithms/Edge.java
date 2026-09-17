b480a7c78ad29063b9507f24fb9ba90595067638
ben-vow-repo <benjaminvowles2008@gmail.com>
2026-09-17 23:54:08 +0100

    feat: Edge constructor and comparer built

--- src/main/java/com/algorithms/Edge.java
+++ src/main/java/com/algorithms/Edge.java
@@ -1,5 +1,15 @@
 package com.algorithms;
 
-public class Edge {
+public class Edge implements Comparable<Edge> {
+    public Node dest;
+    public int weight;
 
+    public Edge(Node dest, int weight){
+        this.dest = dest;
+        this.weight = weight;
+    }
+    @Override
+    public int compareTo(Edge other){
+        return Integer.compare(this.weight, other.weight);
+    }
 }
