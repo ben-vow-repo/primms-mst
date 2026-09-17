316de533e3c7f594a47459a345a5419d21a504d0
ben-vow-repo <benjaminvowles2008@gmail.com>
2026-09-17 23:54:30 +0100

    feat: edge builder created

--- src/main/java/com/algorithms/Main.java
+++ src/main/java/com/algorithms/Main.java
@@ -1,5 +1,12 @@
 package com.algorithms;
 
 public class Main {
+    
 
+    private Main(){}
+
+    public static void createEdge(Node src, Node dest, int weight){
+        src.edgeList.add(new Edge(dest, weight));
+        dest.edgeList.add(new Edge(src, weight));
+    }
 }
