c99047c7712241130bb3dc2691b4724fcbcd5f69
ben-vow-repo <benjaminvowles2008@gmail.com>
2026-09-18 00:03:36 +0100

    feat: sort edge lists on nodes, create node function built

--- src/main/java/com/algorithms/Main.java
+++ src/main/java/com/algorithms/Main.java
@@ -1,12 +1,24 @@
 package com.algorithms;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
-    
+
+    public List<Node> nodes = new ArrayList<>();
 
     private Main(){}
 
     public static void createEdge(Node src, Node dest, int weight){
         src.edgeList.add(new Edge(dest, weight));
         dest.edgeList.add(new Edge(src, weight));
+        Collections.sort(src.edgeList);
+        Collections.sort(dest.edgeList);
+    }
+
+    public static List<Node> createNode(String label, List<Node> nodes){
+        nodes.add(new Node(label));
+        return nodes;
     }
 }
