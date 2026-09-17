4b24c042eeb08d72095412540fa1c834f065b318
ben-vow-repo <benjaminvowles2008@gmail.com>
2026-09-17 23:53:40 +0100

    feat: node constructor built

--- src/main/java/com/algorithms/Node.java
+++ src/main/java/com/algorithms/Node.java
@@ -1,5 +1,14 @@
 package com.algorithms;
 
-public class Node {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Node {
+    public String label;
+    public List<Edge> edgeList;
+
+    public Node(String label){
+        this.label = label;
+        this.edgeList = new ArrayList<>();
+    }
 }
